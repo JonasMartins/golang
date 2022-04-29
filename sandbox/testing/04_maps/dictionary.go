2a0642c0017113c5af4d81fae6467bd076d2112d
JonasMartins <jonas.w.martins@gmail.com>
2022-04-29 11:52:04 -0300

    Changes to be committed: 	modified:   sandbox/testing/04_maps/dictionary.go 	modified:   sandbox/testing/04_maps/dictionary_test.go
            - Learn Go with tests Free Book
            - maps final version.

--- sandbox/testing/04_maps/dictionary.go
+++ sandbox/testing/04_maps/dictionary.go
@@ -1,12 +1,21 @@
 package maps
 
-import "errors"
+const (
 
+    ErrNotFound = DictionaryErr("could not find this word")
+
+    ErrWordExists = DictionaryErr("cannot add word because it already exists")
+
+    ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+
+)
 
 type Dictionary map[string]string
+type DictionaryErr string
 
-
-var ErrNotFound = errors.New("could not find this word")
+func (e DictionaryErr) Error() string {
+    return string(e)
+}
 
 
 func (d Dictionary) Search(word string) (string, error) {
@@ -20,10 +29,36 @@ func (d Dictionary) Search(word string) (string, error) {
     return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-    d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+
+    _, err := d.Search(word)
+    switch err {
+        case ErrNotFound:
+            d[word] = definition
+        case nil:
+            return ErrWordExists
+        default:
+            return err
+    }
+
+    return nil
 }
 
+func (d Dictionary) Update(word, definition string) error {
 
+    _, err := d.Search(word)
+    switch err {
+        case ErrNotFound:
+            return ErrWordDoesNotExist
+        case nil:
+            d[word] = definition
+        default:
+            return err
+    }
+    return nil
+}
 
+func (d Dictionary) Delete(word string) {
+    delete(d, word)
+}
 
