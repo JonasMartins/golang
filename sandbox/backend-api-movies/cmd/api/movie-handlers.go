be26b2ec79f11874304e50c2c0977fd28271bcdb
jonasMartins <jonas.w.martins@gmail.com>
2022-05-19 17:44:00 -0300

    Changes to be committed: 	new file:   sandbox/backend-api-movies/cmd/api/movie-handlers.go 	modified:   sandbox/backend-api-movies/cmd/api/routes.go 	new file:   sandbox/backend-api-movies/cmd/api/utilities.go
            - Retrieving a model.

---
+++ sandbox/backend-api-movies/cmd/api/movie-handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"backend/models"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id := params.ByName("id")
+	if len(id) == 0 {
+		app.logger.Print(errors.New("invalid id"))
+	}
+
+	movie := models.Movie{
+		ID:          "1",
+		Title:       "Test",
+		Description: "test",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 0, 0, 0, 0, time.Local),
+		Runtime:     90,
+		Rating:      5.5,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	app.writeJSON(w, http.StatusOK, movie, "movie")
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
