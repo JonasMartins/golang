53d6f9004545aa4dc43f9aac88bf5989900f54ff
jonasMartins <jonas.w.martins@gmail.com>
2022-06-30 16:47:34 -0300

    micro: 61-65

--- sandbox/mikro/logger-service/cmd/api/main.go
+++ sandbox/mikro/logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -45,6 +47,8 @@ func main() {
 	app := Config{
 		Models: data.New(client),
 	}
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
 
 	//go app.serve()
 	log.Println("Starting sevice on port ", webPort)
@@ -59,21 +63,23 @@ func main() {
 	}
 
 }
-
-/*
-func (app *Config) serve() {
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%s", webPort),
-		Handler: app.routes(),
-	}
-
-	err := srv.ListenAndServe()
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
 	if err != nil {
-		log.Panic()
+		return err
 	}
-}
-*/
+	defer listen.Close()
 
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+
+}
 func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
 	clientOptions.SetAuth(options.Credential{
