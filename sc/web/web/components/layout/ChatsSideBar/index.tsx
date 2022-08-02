ed3c33eec1ef424aedd63eb5d5a7046c1f8d5404
JonasMartins <jonas.w.martins@gmail.com>
2022-08-02 15:32:00 -0300

    SC :
        - Switch chats and hover color and focused chat
        color indicator
    
    Changes to be committed:
        modified:   sc/web/web/components/layout/ChatsSideBar/index.tsx

--- sc/web/web/components/layout/ChatsSideBar/index.tsx
+++ sc/web/web/components/layout/ChatsSideBar/index.tsx
@@ -1,8 +1,11 @@
-import { Paper, Stack, Group, Title, Text } from "@mantine/core";
+import { Paper, Stack, Group, Title, Text, Sx, useMantineColorScheme } from "@mantine/core";
 import type { NextPage } from "next";
 import { ChatType } from "@/features/types/chat";
 import { formatRelative } from "date-fns";
 import { GetChatTitle } from "@/utils/aux/chat.aux";
+import { setFocusedChat } from "@/features/chat/chatSlicer";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/Redux";
 
 interface ChatsSideBarProps {
 	chat: ChatType;
@@ -10,8 +13,38 @@ interface ChatsSideBarProps {
 }
 
 const ChatsSideBar: NextPage<ChatsSideBarProps> = ({ chat, currentUserId }) => {
+	const dispatch = useDispatch();
+	const chatFocused = useSelector((state: RootState) => state.chat.value);
+	const { colorScheme } = useMantineColorScheme();
+	const dark = colorScheme === "dark";
+
+	const lightSx: Sx = {
+		cursor: "pointer",
+		backgroundColor: chatFocused?.base.id === chat.base.id ? "#6ef0f8" : "#fff",
+		"&:hover": {
+			backgroundColor: chatFocused?.base.id === chat.base.id ? "#49dde6" : "#dde0e2",
+		},
+	};
+
+	const darkSx: Sx = {
+		cursor: "pointer",
+		backgroundColor: chatFocused?.base.id === chat.base.id ? "#07858d" : "#1A1B1E",
+		"&:hover": {
+			backgroundColor: chatFocused?.base.id === chat.base.id ? "#07666c" : "#070707",
+		},
+	};
+
 	return (
-		<Paper shadow="md" p="md" radius="md" withBorder>
+		<Paper
+			sx={dark ? darkSx : lightSx}
+			shadow="md"
+			p="md"
+			radius="md"
+			withBorder
+			onClick={() => {
+				dispatch(setFocusedChat(chat));
+			}}
+		>
 			<Stack>
 				<Group grow align="center" position="apart">
 					<Title order={5}>{GetChatTitle(chat, currentUserId)}</Title>
